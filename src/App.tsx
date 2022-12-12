9782adf050df43423f8b05ff8ffbe8e78f792fcc
Md-FarhadHossain <freelancerfarhadhossain@gmail.com>
2022-12-13 03:42:15 +0600

    all files are ready

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@mui/material"
+
+function App() {
+
+
+  return (
+    <div>
+     <h1>hello</h1>
+     <Button variant="text">Text</Button>
+<Button variant="contained">Contained</Button>
+<Button variant="outlined">Outlined</Button> 
+    </div>
+  )
+}
+
+export default App
